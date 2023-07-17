bdb75e247d38fdc0578574eb80a3eaa2904a913e
Sakif Khan <116105234+sakif46425@users.noreply.github.com>
2023-07-17 16:39:49 +0600

    Add files via upload

--- merge two array.cpp
+++ merge two array.cpp
@@ -1,1 +1,46 @@
 #include<bits/stdc++.h>
+using namespace std;
+
+int *mergeTwoArrays(int *A, int n, int *B, int m)
+{
+    int *c = new int[n+m];
+    int i=0, j=0, k=0;
+
+    while(i<=n-1 && j<=m-1)
+    {
+        if(A[i]>B[j])
+        {
+            c[k++]=B[j++];
+        }
+        else
+        {
+            c[k++]=A[i++];
+        }
+    }
+    while(i<=n-1)
+    {
+        c[k++]=A[i++];
+    }
+    while(j<=m-1)
+    {
+        c[k++]=B[j++];
+    }
+
+    return c;
+}
+void print(int *A, int n)
+{
+    for(int i=0; i<n;i++)
+    {
+        cout<<A[i]<<" ";
+    }
+}
+int main()
+{
+    int A[]={5,10,40,50,60};
+    int B[]={11,13,18,99,101};
+    int *c=mergeTwoArrays(A,5,B,5);
+    print(c,10);
+
+}
+
